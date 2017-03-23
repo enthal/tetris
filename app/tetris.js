0fc6614a8ad300505a136b2446c63113bf2e1f5f
Timothy James <tim.james@kindred.ai>
2017-03-23 13:29:13 -0700

    exports first, details after

--- app/tetris.js
+++ app/tetris.js
@@ -3,6 +3,17 @@
 const _ = require('lodash');
 const log = console.log
 
+
+exports.make_piece = (...a) =>
+  _.zipObject(_.split('shapeI orientation x y', / +/), a);
+
+exports.isPieceHere = (piece, gx,gy) =>
+  _.some(shapes[piece.shapeI], ([x,y]) => {
+    [x,y] = [[x,y],[-y,x],[-x,-y],[y,-x]] [piece.orientation % 4];
+    return gx == piece.x+x && gy == piece.y+y;
+  });
+
+
 const stringedShapes = [
   ['xxxx'],
   ['xx',
@@ -25,12 +36,3 @@ const shapes = _.map(stringedShapes, (ss)=> {
   return r;
 });
 log(shapes);
-
-exports.make_piece = (...a) =>
-  _.zipObject(_.split('shapeI orientation x y', / +/), a);
-
-exports.isPieceHere = (piece, gx,gy) =>
-  _.some(shapes[piece.shapeI], ([x,y]) => {
-    [x,y] = [[x,y],[-y,x],[-x,-y],[y,-x]] [piece.orientation % 4];
-    return gx == piece.x+x && gy == piece.y+y;
-  });
