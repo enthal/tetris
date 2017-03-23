5dab7cfd288c765ae8e5db7b252f099eab894435
Timothy James <tim.james@kindred.ai>
2017-03-22 17:13:12 -0700

    isPieceHere

--- tetris.js
+++ tetris.js
@@ -9,14 +9,14 @@ const _=require('lodash');
 const shapes = [
   [[0,0],[1,0],[2,0],[3,0],],
   [[0,0],[1,0],
-   [1,0],[1,1],],
+   [0,1],[1,1],],
 ];
 
 const make_piece = (...a) =>
   _.zipObject(_.split('shapeI orientation x y', / +/), a);
 const pieces = [
   make_piece(0,0,5,2),
-  make_piece(0,1,2,3),
+  make_piece(0,1,2,4),
   make_piece(1,0,1,7),
   make_piece(1,1,7,9),
 ];
@@ -30,6 +30,10 @@ const overBoard = (cellF, rowEndF) => {
     if (rowEndF) rowEndF(y);
   }
 }
+const isPieceHere = (piece, gx,gy) =>
+  _.some(shapes[piece.shapeI], ([x,y]) =>
+    gx == piece.x+x && gy == piece.y+y  );
+
 
 const render = () => {
   for (const piece of pieces) {
@@ -37,7 +41,7 @@ const render = () => {
   }
   overBoard(
     (x,y) => {
-      piece = _.find(pieces, (piece) => x==piece.x && y==piece.y);
+      piece = _.find(pieces, (piece) => isPieceHere(piece, x,y));
       write(piece ? ''+piece.shapeI : '·');
     },
     () => write('\n')
