6e17f0a7fdc4f3c923ff292b2b846127a0470e0d
Timothy James <tim.james@kindred.ai>
2017-03-24 12:17:36 -0700

    functionalize

--- app/tetris.js
+++ app/tetris.js
@@ -18,18 +18,18 @@ const getShapeSize = (shape) =>
 const getPieceCellCoordinates =
 exports.getPieceCellCoordinates = (piece) => {
   const shape = shapes[piece.shapeI];
-  let mx,my; [mx,my] = getShapeSize(shape);
-  return _.map(shape, ([x,y]) => {
-    x -= mx/2;
-    y -= my/2;
-    [x,y] = rotateCoords([x,y], piece.orientation);
-    x = _.floor(x + mx/2);
-    y = _.floor(y + my/2);
-    return [piece.x+x, piece.y+y];
+  const size = getShapeSize(shape);
+  return _.map(shape, (xy) => {
+    xy = _.zipWith(size, xy, (s,c) => c - s/2)
+    xy = rotateCoords(piece.orientation, xy);
+    xy = _.zipWith(size, xy, (s,c) => _.floor(c + s/2))
+    return [
+      piece.x+xy[0],
+      piece.y+xy[1] ];
   });
 }
 
-const rotateCoords = ([x,y], orientation) =>
+const rotateCoords = (orientation, [x,y]) =>
   [[x,y],[-y,x],[-x,-y],[y,-x]] [orientation % 4]  // clockwise, around origin
 
 exports.isPieceHere = (piece, gx,gy) =>
