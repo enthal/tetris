5dc63e33e9a58eeee0bb0de4ab77d8952da708dc
Timothy James <tim.james@kindred.ai>
2017-03-26 18:53:24 -0700

    extract makeView, convertToCells (both data and SVG rects); CSS transition on cell destroying

--- app/index.js
+++ app/index.js
@@ -3,31 +3,68 @@ const tetris = require('./tetris');
 const log = console.log
 
 let livePiece;
-let deadCells = [];
+const deadCells = [];
 const maxX = 10;
 const maxY = 20;
 
-const render = () => {
-  const svg = document.querySelector("svg #game");
-  const svgns = "http://www.w3.org/2000/svg";
+const makeView = (gameElem) => {
+  const liveCellsGroupElem = gameElem.querySelector('g#live-cells');
+  const deadCellsGroupElem = gameElem.querySelector('g#dead-cells');
+  const svgns = 'http://www.w3.org/2000/svg';
 
-  while (svg.firstChild) {
-    svg.removeChild(svg.firstChild);
+  gameElem.addEventListener('transitionend', (event) => {
+    if (event.target.classList.contains('destroying')) {
+      event.target.parentElement.removeChild(event.target);
+    }
+    // TODO: this will not fire for the cells just appended to deadCellsGroupElem; maybe on next tick it would?  WTF?
+  });
+
+  const generateCellSvgRect = (parentElem, [x,y], shapeI) => {
+    const rect = document.createElementNS(svgns,'rect');
+    rect.setAttribute('width',1);
+    rect.setAttribute('height',1);
+    rect.setAttribute('x',x);
+    rect.setAttribute('y',y);
+    rect.setAttribute('class',`shape-${shapeI}`);
+    parentElem.appendChild(rect);
+    return rect;
   }
 
-  const shapeCells = _.concat(
-    deadCells,
-    _.map(tetris.getPieceCellCoordinates(livePiece), (coords)=>[coords,livePiece.shapeI]) )
-  _.each(shapeCells, ([[x,y],shapeI]) => {
-    const rect = document.createElementNS(svgns,"rect");
-    rect.setAttribute("width",1);
-    rect.setAttribute("height",1);
-    rect.setAttribute("x",x);
-    rect.setAttribute("y",y);
-    rect.setAttribute("class",`shape-${shapeI}`);
-    svg.appendChild(rect);
-  });
-};
+  const convertToCells = (parentElem, piece) => _.map(
+    tetris.getPieceCellCoordinates(piece),
+    (xy) => {
+      const elem = generateCellSvgRect(parentElem, xy, piece.shapeI);
+      return {
+        getY:  () => xy[1],
+        getXY: () => xy,
+        moveBy: (dxy) => {
+          xy = _.zipWith(xy, dxy, (c,dc) => c + dc);
+          elem.setAttribute('x', xy[0]);
+          elem.setAttribute('y', xy[1]);
+        },
+        destroy: () => {  // TODO
+          elem.setAttribute('transform', `rotate(45 ${xy[0]+0.5} ${xy[1]+0.5})`)
+          elem.classList.add('destroying');
+        },
+      };
+    })
+
+  const removeAllChildren = (p) => {
+    while (p.firstChild) { p.removeChild(p.firstChild); }
+  }
+
+  return {
+    renderDeadCells: (piece) => {
+      deadCells.push(...convertToCells(deadCellsGroupElem, piece));
+    },
+    renderLivePiece: () => {
+      removeAllChildren(liveCellsGroupElem);
+      convertToCells(liveCellsGroupElem, livePiece);
+    },
+  };
+}
+
+const view = makeView(document.querySelector("svg #game"));
 
 let currentTimeout;
 const onTick = () => {
@@ -54,37 +91,46 @@ document.addEventListener('keydown', (event) => {
   if (!isPieceOverlapping(maybeLivePiece) && !isPieceOutOfBounds(maybeLivePiece)) {
     livePiece = maybeLivePiece;
   }
-  render();
+  view.renderLivePiece();
 });
 
 const doStep = () => {
   if (!livePiece) {
     livePiece = tetris.makePiece(_.random(6),0,4,-1);
     log("new livePiece", livePiece)
+
     if (isPieceOverlapping(livePiece)) {
       log("GAME OVER");
-      deadCells = [];
+      _.each(deadCells, (o) => o.destroy());
+      deadCells.length = 0;
     }
   }
 
   if (isLivePieceOnBottom() || isPieceOverlapping(livePiece, [0,1])) {
-    deadCells.push(..._.map(tetris.getPieceCellCoordinates(livePiece), (coords)=>[coords,livePiece.shapeI]));
+    view.renderDeadCells(livePiece);
     livePiece = null;
 
-    const yCounts = _.countBy(deadCells, ([[x,y], __]) => y);
+    const yCounts = _.countBy(deadCells, (o) => o.getY());
     const killedYs = _.transform(yCounts,
       (r,count,y) => { if (count==10) r.push(+y); },
       []);
-    _.remove(deadCells, ([[ign1,y],ign2]) => _.some(killedYs, (killedY) => y == killedY));
+
+    _.remove(deadCells, (deadCell) => {
+      if (_.some(killedYs, (killedY) => deadCell.getXY()[1] == killedY)) {
+        deadCell.destroy();
+        return true;
+      }
+    });
+
     _.each(killedYs, (killedY) =>
       _.each(deadCells, (deadCell) => {
-        if (deadCell[0][1] < killedY) deadCell[0][1]++
+        if (deadCell.getY() < killedY) deadCell.moveBy([0,1]);
       }));
 
     doStep();
   } else {
     livePiece.y += 1;
-    render();
+    view.renderLivePiece();
   }
 }
 
@@ -93,9 +139,9 @@ const isLivePieceOnBottom = () =>
 
 const isPieceOverlapping = (piece, offsetXY=[0,0]) =>
   _.some(tetris.getPieceCellCoordinates(piece), (pieceXY) =>
-    _.some(deadCells, ([deadXY], ignore) =>
-      pieceXY[0] + offsetXY[0] == deadXY[0] &&
-      pieceXY[1] + offsetXY[1] == deadXY[1] ));
+    _.some(deadCells, (deadCell) =>  // TODO: move to cell
+      pieceXY[0] + offsetXY[0] == deadCell.getXY()[0] &&
+      pieceXY[1] + offsetXY[1] == deadCell.getXY()[1] ));
 
 const isPieceOutOfBounds = (piece) =>
   _.some(tetris.getPieceCellCoordinates(piece), ([x,y]) =>
